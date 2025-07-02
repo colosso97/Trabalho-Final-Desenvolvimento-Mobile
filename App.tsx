e8d71f58ff0d3f570008c913f9d3e660374d72a7
ThaynaL <cristinathayna06@gmail.com>
2025-07-02 09:12:01 -0300

    Retirando importações a mais

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { View, Text, StatusBar } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import AppRouter from './src/routes'
 import { AuthProvider } from './src/context/AuthContext'
